53dc23cc3155fbcc033b539ed7e540d74c13d14a
Archelangelo <medycc@gmail.com>
2016-03-10 10:16:43 -0500

    Raman datatype file added

---
+++ RamanData.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    Defines the basic datatype used to store Raman spectra from
+    Horiba Jobin-Yvon LabRam.
+
+    This is part of a Raman spectrum analysis toolkit.
+
+    Yiran Hu (yiranhu@gatech.edu)
+    Epitaxial Graphene Lab
+    School of Physics
+    Georgia Tech
+'''
+
+import numpy as np
+
+class RamanData():
+    
