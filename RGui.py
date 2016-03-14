5b7e47316d4fca2bb8358ff1d2afcecd1705f663
Archelangelo <medycc@gmail.com>
2016-03-14 00:54:57 -0400

    GUI list signals connected to the plot slot

--- RGui.py
+++ RGui.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python3
+# Defines the GUI of Raman Kit
+#
+# This is part of a Raman spectrum analysis toolkit.
+#
+# Yiran Hu (yiranhu@gatech.edu)
+# Epitaxial Graphene Lab
+# School of Physics
+# Georgia Tech
+
 import sys
 import os.path
 import numpy as np
 import matplotlib
 matplotlib.use("Qt5Agg")
 import RamanData as rd
-from PyQt5 import QtCore
+from PyQt5 import QtCore, Qt
 from PyQt5.QtWidgets import (QMainWindow, QAction, QFileDialog, QApplication,
-    QBoxLayout, QSizePolicy, QWidget, QTabWidget, QListWidget)
+    QBoxLayout, QSizePolicy, QWidget, QTabWidget, QListWidget, QListWidgetItem)
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
@@ -53,7 +63,7 @@ class RGui(QMainWindow):
         self.statusBar()
 
     def openFile(self):
-        fileName = QFileDialog.getOpenFileName(self, "Open file", '/home')
+        fileName = QFileDialog.getOpenFileName(self, "Open file", '/home/workspace/RamanKit/')
         if fileName[0]:
             self.spec.append(rd.SpecData(fileName[0]))
 
@@ -65,9 +75,19 @@ class RGui(QMainWindow):
             self.selectPane.addTab(listWidget, os.path.basename(fileName[0]))
 
         for i in range(self.spec[-1]._coord.shape[0]):
-            listWidget.addItem("[%d %d %d]" % tuple(self.spec[-1]._coord[i]))
+            newItem = QListWidgetItem("[%d %d %d]" % \
+                tuple(self.spec[-1]._coord[i]), listWidget)
+            newItem.setData(0x0100, QtCore.QVariant([len(self.spec) - 1, i]))
+            listWidget.addItem(newItem)
+        listWidget.itemDoubleClicked.\
+            connect(self.updatePlot)
 
-        self.canvas.updatePlot(self.spec[-1])
+    def updatePlot(self, item):
+        bArray = item.data(0x0100)
+        n = int(bArray[0])
+        i = int(bArray[1])
+        use = np.array(i).reshape(-1)
+        self.canvas.updatePlot(self.spec[n], use)
 
 class CanvasWidget(FigureCanvas):
 
@@ -82,8 +102,8 @@ class CanvasWidget(FigureCanvas):
         super().setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         super().updateGeometry()
 
-    def updatePlot(self, spec):
-        self.axes.plot(spec._data[[0]].T, spec._data[1:].T)
+    def updatePlot(self, spec, use):
+        self.axes.plot(spec._data[[0]].T, spec._data[use].T)
         self.draw()
 
 if __name__ == '__main__':
