5128b15a53e1d51976743083da4bf17a89356c4c
Archelangelo <medycc@gmail.com>
2016-03-21 10:56:50 -0400

    temporary commit

--- RGui.py
+++ RGui.py
@@ -14,14 +14,14 @@ import numpy as np
 import matplotlib
 matplotlib.use("Qt5Agg")
 import RamanData as rd
-from PyQt5 import QtCore
+from PyQt5.QtCore import Qt, QVariant, QSettings, QRegExp
 from PyQt5.QtWidgets import (QMainWindow, QAction, QFileDialog, QApplication,
     QSplitter, QSizePolicy, QFrame, QTabWidget, QListWidget, QListWidgetItem,
-    QHBoxLayout, QPushButton, QGridLayout, QAbstractItemView, QLineEdit, QLabel)
+    QHBoxLayout, QPushButton, QGridLayout, QAbstractItemView, QLineEdit, QLabel,
+    QCheckBox)
 from PyQt5.QtGui import QRegExpValidator
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
 
 class RGui(QMainWindow):
 
@@ -31,18 +31,18 @@ class RGui(QMainWindow):
 
     def initUI(self):
         # Main window set up
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setAttribute(Qt.WA_DeleteOnClose)
         self.setWindowTitle("RGui")
 
         # File menu and Open action
         self.fileMenu = self.menuBar().addMenu('&File')
         self.fileMenu.addAction('&Open', self.openFile,
-            QtCore.Qt.CTRL + QtCore.Qt.Key_O)
+            Qt.CTRL + Qt.Key_O)
 
         # Main widget and its layout
         # subWidget is embedded in the right half of mainWidget
-        self.mainWidget = QSplitter(QtCore.Qt.Horizontal, self)
-        self.subWidget = QSplitter(QtCore.Qt.Vertical, self)
+        self.mainWidget = QSplitter(Qt.Horizontal, self)
+        self.subWidget = QSplitter(Qt.Vertical, self)
 
         # Plot canvas set up, added to layout
         self.canvas = CanvasWidget(self,
@@ -82,10 +82,15 @@ class RGui(QMainWindow):
         backSubButton.clicked.connect(self.backSub)
         plotButton = QPushButton('Plot', mainCtr)
         plotButton.clicked.connect(self.updatePlot1)
+        plotPeak = QCheckBox('Plot fitted peak', mainCtr)
+        holdPlot = QCheckBox('Hold plot', mainCtr)
+        holdPlot.stateChanged.connect(self.canvas.toggleHold)
         # layout
         mainLayout = QGridLayout(mainCtr)
         mainLayout.addWidget(backSubButton, 0, 0)
         mainLayout.addWidget(plotButton, 0, 1)
+        mainLayout.addWidget(plotPeak, 1, 0)
+        mainLayout.addWidget(holdPlot, 1, 1)
         mainCtr.setLayout(mainLayout)
 
         self.ctrPane.addTab(mainCtr, 'Main Ctrol')
@@ -130,31 +135,33 @@ class RGui(QMainWindow):
         for i in range(self.spec[-1]._coord.shape[0]):
             newItem = QListWidgetItem("[%d %d %d]" % \
                 tuple(self.spec[-1]._coord[i]), listWidget)
-            newItem.setData(QtCore.Qt.UserRole,
-                QtCore.QVariant([len(self.spec) - 1, i]))
+            newItem.setData(Qt.UserRole,
+                QVariant([len(self.spec) - 1, i]))
             listWidget.addItem(newItem)
         listWidget.itemDoubleClicked.connect(self.updatePlot2)
 
     # defines two update plot slots
     def updatePlot1(self):
+        # slot for multiple items
         items = self.selectPane.currentWidget().selectedItems()
         if not items:
             pass # raise error
-        bArray = items[0].data(QtCore.Qt.UserRole)
+        bArray = items[0].data(Qt.UserRole)
         n = int(bArray[0])
         use = []
         for item in items:
-            bArray = item.data(QtCore.Qt.UserRole)
+            bArray = item.data(Qt.UserRole)
             use.append(int(bArray[1]))
         use = np.array(use).reshape(-1)
-        self.canvas.updatePlot(self.spec[n], use)
+        self.canvas.updatePlot(self.spec[n], use, plotPeak)
 
     def updatePlot2(self, item):
-        bArray = item.data(QtCore.Qt.UserRole)
+        # slot for a single item
+        bArray = item.data(Qt.UserRole)
         n = int(bArray[0])
         i = int(bArray[1])
         use = np.array(i).reshape(-1)
-        self.canvas.updatePlot(self.spec[n], use)
+        self.canvas.updatePlot(self.spec[n], use, plotPeak)
 
     # Here comes the data analysis operations
 
@@ -167,16 +174,16 @@ class RGui(QMainWindow):
             bg = rd.SpecData(fileName[0])
             listWidget = self.selectPane.currentWidget()
             item = listWidget.item(0)
-            n = item.data(QtCore.Qt.UserRole)[0]
+            n = item.data(Qt.UserRole)[0]
             newSpec = self.spec[n].backSub(bg.getSpec(0))
-            self.addSpec(newSpec, self.currentTabText + '_subtracted')
+            self.addSpec(newSpec, self.currentTabTitle() + '_subtracted')
 
     def NMF(self):
         if self.firstPlot:
             pass # raise an error
 
         item = self.selectPane.currentWidget().item(0)
-        n = item.data(QtCore.Qt.UserRole)[0]
+        n = item.data(Qt.UserRole)[0]
         alpha = float(self.alphaBox.text())
         l1Ratio = float(self.l1RatioBox.text())
         model = self.spec[n].NMF(n_components = 3, init = 'nndsvd',
@@ -188,6 +195,21 @@ class RGui(QMainWindow):
         newSpec._dim = np.array([1, 1, 3])
         self.addSpec(newSpec, self.currentTabTitle() + '_NMF')
 
+        # save settings
+        self.saveSettings()
+
+    def saveSettings(self):
+        settings = QSettings(QSettings.UserScope)
+        settings.setValue('alpha', self.alphaBox.text())
+        settings.setValue('l1Ratio', self.l1RatioBox.text())
+
+    def loadSetting(self):
+        settings = QSettings(QSettings.UserScope)
+        if settings.contains('alpha'):
+            self.alphaBox.setText(settings.value('alpha'))
+        if settings.contains('l1Ratio'):
+            self.l1RatioBox.setText(settings.value('l1Ratio'))
+
     def currentTabTitle(self):
         return self.selectPane.tabText(self.selectPane.currentIndex())
 
@@ -196,7 +218,7 @@ class MyDoubleBox(QLineEdit):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        re = QtCore.QRegExp('([0-9]*\.[0-9]+|[0-9]+\.?[0-9]*)([eE]-?[0-9]+)?')
+        re = QRegExp('([0-9]*\.[0-9]+|[0-9]+\.?[0-9]*)([eE]-?[0-9]+)?')
         myRegValidator = QRegExpValidator(re, self)
         self.setValidator(myRegValidator)
 
@@ -213,12 +235,27 @@ class CanvasWidget(FigureCanvas):
         super().setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         super().updateGeometry()
 
-    def updatePlot(self, spec, use):
+        self.base = 0
+        settings = QSettings(QSettings.UserDomain)
+        if settings.contains('ele'):
+            self.ele = float(settings.value('ele'))
+        else:
+            settings.setValue('ele', 20)
+            self.ele = 20
+
+    def updatePlot(self, spec, use, plotPeak = False):
+        
         self.axes.plot(spec._data[[0]].T, spec._data[use + 1].T)
         self.draw()
 
+    def toggleHold(self, state):
+        self.axes.hold(state)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setApplicationName('RamanGui')
+    app.setOrganizationName('Georgia Tech')
+    app.setOrganizationDomain('www.graphene.gatech.edu')
 
     mw = RGui()
     mw.setWindowTitle("Raman Gui")
